bb7fe1d59f39d6723aa42b7baa6fadb04f268dc7
Joshua Figueroa <joshuavillartafigueroa@gmail.com>
2024-07-30 04:20:43 +0800

    Reduce starts count

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -7,7 +7,7 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import * as random from "maath/random/dist/maath-random.esm";
 const Stars = () => {
 	const ref = useRef<ThreePoints<BufferGeometry, Material>>(null);
-	const [sphere] = useState(() => random.inSphere(new Float32Array(6000), { radius: 1.2 }));
+	const [sphere] = useState(() => random.inSphere(new Float32Array(600), { radius: 1.2 }));
 
 	useFrame((_, delta) => {
 		if (ref.current) {
